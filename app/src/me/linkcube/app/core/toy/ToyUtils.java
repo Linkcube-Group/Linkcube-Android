285b6d598605cbf6b7f1d22ded6e58cbba97aeb9
shangcongrong <yangxin5693575@163.com>
2014-07-14 17:13:02 +0800

    删掉了部分无用的代码

--- app/src/me/linkcube/app/core/toy/ToyUtils.java
+++ app/src/me/linkcube/app/core/toy/ToyUtils.java
@@ -7,8 +7,6 @@ package me.linkcube.app.core.toy;
  * 
  */
 public class ToyUtils {
-	private final static int KModeNum = 8;
-
 	/**
 	 * 波形信号
 	 * 
@@ -18,17 +16,10 @@ public class ToyUtils {
 	 */
 	public static long computeWaveLevel(byte[] waveform) {
 		// return s == null || s.compareTo("") == 0;
-		int v = 0;
 		long sound = 0;
 		for (int nc = 0; nc < waveform.length; nc++) {
 			int tmp = (int) waveform[nc] + (int) 128;
-			// int tmp = Math.abs((int)waveform[nc]);
 			sound += tmp;
-			/*
-			 * byte bv = (byte)waveform[nc]; short sv = (short)waveform[nc]; int
-			 * iv = (int)waveform[nc]; if(iv==-128) { iv++; } else { iv--; }
-			 */
-			// v/=waveform.length;
 		}
 		sound /= waveform.length;
 		return sound;
@@ -42,36 +33,13 @@ public class ToyUtils {
 	 * @return long
 	 */
 	public static long computeFFTLevel(byte[] fftform) {
-		// return s == null || s.compareTo("") == 0;
 		long waveng = 0;
-		double energy = 0.f;
-
 		for (int nc = 0; nc < fftform.length; nc++) {
-			/*
-			 * int num = nc/64; value[num] += fftform[nc]*fftform[nc];
-			 */
 			int value;
 			value = (int) fftform[nc];
 			value = Math.abs(value);
-			// waveng+=Math.abs(fftform[nc]);
 			waveng += value;
-			energy += (double) value;
 		}
-		// waveng/=fftform.length;
-		// energy/=(double)fftform.length;
 		return waveng;
-
-		/*
-		 * long[] value = {0,0,0,0,0,0,0,0}; long tmp; int nc,nmax;
-		 * 
-		 * for(nc=0;nc<fftform.length;nc++) { int num = nc/64; value[num] +=
-		 * fftform[nc]*fftform[nc]; }
-		 * 
-		 * nmax=nc=0; tmp=value[nc]; for(nc=0;nc<KModeNum;nc++) {
-		 * if(value[nc]>tmp) { tmp = value[nc]; nmax = nc; } }
-		 * 
-		 * if(nmax==KModeNum-1) { nmax=40; } else if(nmax==0) { nmax=1; } else {
-		 * nmax=nmax*5+5; } return nmax;
-		 */
 	}
 }
--- app/src/me/linkcube/app/sync/chat/GetOfflineMessage.java
+++ app/src/me/linkcube/app/sync/chat/GetOfflineMessage.java
@@ -11,39 +11,43 @@ import org.jivesoftware.smackx.OfflineMessageManager;
 
 import android.content.Context;
 import android.util.Log;
+
 /**
  * 获取离线消息，目前没有用到2014-3-11
+ * 
  * @author Rodriguez-xin
- *
+ * 
  */
 public class GetOfflineMessage {
 
-	private String TAG="GetOfflineMessage";
+	private String TAG = "GetOfflineMessage";
 	private Context context;
 	private OfflineMessageManager msgManager;
-	public GetOfflineMessage(Context _context){
-		this.context=_context;
-		msgManager=new OfflineMessageManager( ASmackManager.getInstance().getXMPPConnection());
+
+	public GetOfflineMessage(Context _context) {
+		this.context = _context;
+		msgManager = new OfflineMessageManager(ASmackManager.getInstance()
+				.getXMPPConnection());
 	}
-	public void getOfflineMsg(){
-		String userName=ASmackUtils.getRosterName();
+
+	public void getOfflineMsg() {
+		String userName = ASmackUtils.getRosterName();
 		try {
-			//System.out.println(msgManager.supportsFlexibleRetrieval());
-			Log.i(TAG, "msgcount:"+msgManager.getMessageCount());
-			//System.out.println("msgcount:"+msgManager.getMessageCount());
-			Iterator<Message> iterator=msgManager.getMessages();
-			while(iterator.hasNext()){
-				Message msg=iterator.next();
-				Log.i(TAG, "from:"+msg.getFrom()+"body:"+msg.getBody());
+			// System.out.println(msgManager.supportsFlexibleRetrieval());
+			Log.i(TAG, "msgcount:" + msgManager.getMessageCount());
+			// System.out.println("msgcount:"+msgManager.getMessageCount());
+			Iterator<Message> iterator = msgManager.getMessages();
+			while (iterator.hasNext()) {
+				Message msg = iterator.next();
+				Log.i(TAG, "from:" + msg.getFrom() + "body:" + msg.getBody());
 			}
 			msgManager.deleteMessages();
-			
+
 		} catch (XMPPException e) {
 			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		
+
 	}
 }
