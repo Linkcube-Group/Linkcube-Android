7817508e30893d7776f11271cec1832601455573
Ervin Wang <orange.work@qq.com>
2014-08-30 00:26:54 +0800

    简化引导页的实现逻辑

--- Linkcube/src/me/linkcube/app/ui/welcome/GuideActivity.java
+++ Linkcube/src/me/linkcube/app/ui/welcome/GuideActivity.java
@@ -1,9 +1,19 @@
 package me.linkcube.app.ui.welcome;
 
+import static me.linkcube.app.AppConfig.GUIDE_IMAGES_RES;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import me.linkcube.app.R;
+import me.linkcube.app.ui.BasePagerAdapter;
 import me.linkcube.app.ui.DialogActivity;
 import me.linkcube.app.ui.main.MainActivity;
 
@@ -13,30 +23,59 @@ import me.linkcube.app.ui.main.MainActivity;
  * @author Orange
  * 
  */
-public class GuideActivity extends DialogActivity implements
-		OnGuideCompleteListener {
+public class GuideActivity extends DialogActivity  {
 	private ViewPager viewPager;
 
-	private GuidePagerAdapter adapter;
+	private BasePagerAdapter adapter;
+
+	private List<View> viewList;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.guide_activity);
 		setUpViews();
-		adapter.setOnCompleteListener(this);
 		viewPager.setAdapter(adapter);
 	}
 
 	private void setUpViews() {
+		getGuideViewList();
 		viewPager = (ViewPager) findViewById(R.id.viewPagerGuide);
-		adapter = new GuidePagerAdapter(this);
+		adapter = new BasePagerAdapter(this, viewList);
 
 	}
 
-	@Override
-	public void onComplete() {
-		startActivity(new Intent(GuideActivity.this, MainActivity.class));
-		finish();
+	private void getGuideViewList() {
+		viewList = new ArrayList<View>();
+		for (int i = 0; i < GUIDE_IMAGES_RES.length; i++) {
+			View view = getImageView(GUIDE_IMAGES_RES[i]);
+			view.setOnClickListener(new OnGuideViewClick(i));
+			viewList.add(i, view);
+		}
+	}
+
+	private ImageView getImageView(int resId) {
+		ImageView imageView = new ImageView(this);
+		imageView.setScaleType(ScaleType.CENTER_CROP);
+		imageView.setImageResource(resId);
+		return imageView;
+	}
+
+	private class OnGuideViewClick implements OnClickListener {
+
+		private int mPosition;
+
+		public OnGuideViewClick(int position) {
+			mPosition = position;
+		}
+
+		@Override
+		public void onClick(View v) {
+			if (mPosition == GUIDE_IMAGES_RES.length - 1) {
+				startActivity(new Intent(GuideActivity.this, MainActivity.class));
+				finish();
+			}
+		}
+
 	}
 }
